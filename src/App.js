fba823ee21e0e0a5f7a8718af199825c678692b4
Josh Duncan <josh.duncan.dev@gmail.com>
2019-06-22 23:16:57 -0600

    using environment in app, styling updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Environment from './Components/Environment'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'block'}}>
+      <div className='container'>
+        <button>
+          Completely Random
+        </button>
+        <button>
+          Guided Creation
+        </button>
+      </div>
+      <div className='container'>
+        <Environment/>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
