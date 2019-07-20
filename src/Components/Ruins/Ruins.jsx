6899e3c69cf2e2b3d48247f7f394e5b28e60c93c
Josh Duncan <josh.duncan.dev@gmail.com>
2019-07-20 15:11:58 -0600

    updated ruins to use Selector

--- src/Components/Ruins/Ruins.jsx
+++ src/Components/Ruins/Ruins.jsx
@@ -1,67 +1,34 @@
-import React, {Component} from 'react'
-import RuinsLogic from './Ruins.logic'
-import DiceRoller from '../../DiceRoller/DiceRoller'
+import React, {Component, Fragment} from 'react'
+import ruinsData from './Ruins.data'
+import Selector from '../Selector/Selector';
+import { LineBreak } from '../../Utils/Utils';
 
 class Ruins extends Component {
     state = {
-        random: this.props.random,
-        shouldSelect: false,
-        selected: undefined
+        ruinsType: ''
     }
 
-    onRandomNormalRuins = () => {
-        this.setState({random: 'normal'});
-    }
-
-    onSelectNormalRuins = () => {
-        this.setState({shouldSelect: 'normal'});
-    }
-
-    onRandomIndustryRuins = () => {
-        this.setState({random: 'industry'});
-    }
-
-    onSelectIndustryRuins = () => {
-        this.setState({shouldSelect: 'industry'});
-    }
-
-    onRuinsChosen = ({target}) => {
-        this.setState({selected: target.innerText})
+    setRuinsType = ({target}) => {
+        this.setState({ruinsType: target.innerText});
     }
 
     render() {
-        let content = (
-            <React.Fragment>
-                <button onClick={this.onRandomNormalRuins}>Random Normal Ruins</button>
-                <button onClick={this.onSelectNormalRuins}>Select Normal Ruins</button>
-                <button onClick={this.onRandomIndustryRuins}>Random Industry Ruins</button>
-                <button onClick={this.onSelectIndustryRuins}>Select Industry Ruins</button>
-            </React.Fragment>)
-        if (this.state.random) {
-            const logic = this.state.random === 'normal' ? RuinsLogic.normal : RuinsLogic.industry;
-            const {ruins, roll} = logic.getRandomRuins();
-            content = <div>{`${DiceRoller.toString(roll)}: ${ruins}`}</div>
-        } else if (this.state.selected) {
-            content = <div>{this.state.selected}</div>
-        } else if (this.state.shouldSelect) {
-            const logic = this.state.shouldSelect === 'normal' ? RuinsLogic.normal : RuinsLogic.industry;
-            const buttons = logic.distinctTypes.map((type, idx) => {
-                return (<button key={idx} onClick={this.onRuinsChosen}>{type}</button>)
-            });
-            content = <React.Fragment>
-                    <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
-                    {buttons}
-                    </div>
-                </React.Fragment>   
-        }
-
-        return (
-            <React.Fragment>
-                <h1>Ruins:</h1>
-                <div style={{width:'100%'}}/>
-                {content}
-            </React.Fragment>
-        )
+        return <Fragment>
+            {!this.state.ruinsType &&
+                <Fragment>
+                    <h1>Ruins:</h1>
+                    <LineBreak />
+                    <button onClick={this.setRuinsType}>Normal</button>
+                    <button onClick={this.setRuinsType}>Industry</button>
+                </Fragment>
+            }
+            {this.state.ruinsType === 'Normal' && 
+                <Selector title="Ruins" data={ruinsData.normal} />
+            }
+            {this.state.ruinsType === 'Industry' && 
+                <Selector title="Ruins" data={ruinsData.industry} />
+            }
+            </Fragment> 
     }
 }
 
